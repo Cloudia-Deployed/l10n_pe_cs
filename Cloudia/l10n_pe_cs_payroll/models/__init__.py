90b622d17a385ace470a5a2db2ee3707eeae0b92
Alvaro <alvaro@catalytic.pe>
2023-01-01 18:32:22 -0500

    FIX

--- Cloudia/l10n_pe_cs_payroll/models/__init__.py
+++ Cloudia/l10n_pe_cs_payroll/models/__init__.py
@@ -5,3 +5,10 @@
 
 from . import hr_payslip
 from . import hr_rule_parameter
+from . import hr_contract
+from . import hr_income_tax
+from . import hr_plan_health
+from . import hr_plan_life
+from . import hr_plan_retirement
+from . import hr_union_plan
+from . import hr_union
